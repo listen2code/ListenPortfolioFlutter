52bd15079e2c79d9473aa0109b3b5b3ce3ad9d74
listen2code@gmail.com <listen2code@gmail.com>
2026-01-31 10:08:17 +0900

    update: implement Android 12+ Splash Screen API and styles

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,7 +1,6 @@
 plugins {
     id("com.android.application")
     id("kotlin-android")
-    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
 }
 
@@ -20,10 +19,7 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.listen.portfolio.listen_portfolio_flutter"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
@@ -32,11 +28,17 @@ android {
 
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
             signingConfig = signingConfigs.getByName("debug")
         }
     }
+}
+
+dependencies {
+    // Because Android 12+ introduced a new Splash Screen API.
+    // On Android 12 and higher, the system ignores the old windowBackground settings
+    // and instead uses the new mandatory launch screen. If not properly adapted,
+    // it will display the system's default dark (or white) background.
+    implementation("androidx.core:core-splashscreen:1.0.1")
 }
 
 flutter {
--- android/app/src/main/kotlin/com/listen/portfolio/listen_portfolio_flutter/MainActivity.kt
+++ android/app/src/main/kotlin/com/listen/portfolio/listen_portfolio_flutter/MainActivity.kt
@@ -1,5 +1,18 @@
 package com.listen.portfolio.listen_portfolio_flutter
 
+import android.os.Bundle
+import androidx.core.view.WindowCompat
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity : FlutterActivity()
+class MainActivity : FlutterActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        // 1. Call installSplashScreen() before super.onCreate
+        installSplashScreen()
+
+        super.onCreate(savedInstanceState)
+
+        // 2. Ensure that the content can extend to the system bar (optional, to enhance visual consistency)
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+    }
+}
