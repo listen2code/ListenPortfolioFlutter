a7f715631db1b73e8335ea8c8a49065bd3625c36
listen2code@gmail.com <listen2code@gmail.com>
2026-02-28 21:47:00 +0900

    refactor: use centralized core export in `base_provider.dart`
    - Update `lib/core/base/base_provider.dart` to use the `core.dart` barrel file import instead of a direct import for `BaseEffect`.

--- lib/core/base/base_provider.dart
+++ lib/core/base/base_provider.dart
@@ -1,4 +1,4 @@
-import 'package:listen_portfolio_flutter/core/base/base_effect.dart';
+import 'package:listen_portfolio_flutter/core/core.dart';
 
 /// A central registry for managing global provider implementations and effect routing.
 class ProviderRegistry {
