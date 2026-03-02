ac7b4add0fe5e5563eae35b1de0b5e460b0a4ca6
listen2code@gmail.com <listen2code@gmail.com>
2026-03-02 22:13:56 +0900

    feat: add `listen_core` dependency and clean up imports
    - Add `listen_core` as a Git dependency from the `ListenCoreFlutter` repository.
    - Remove unused `flutter_secure_storage` import from `auth_provider.dart`.

--- lib/features/auth/presentation/provider/auth_provider.dart
+++ lib/features/auth/presentation/provider/auth_provider.dart
@@ -1,5 +1,4 @@
 import 'package:connectivity_plus/connectivity_plus.dart';
-import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:listen_portfolio_flutter/core/core.dart';
 import 'package:listen_portfolio_flutter/features/auth/data/datasources/auth_local_data_source.dart';
 import 'package:listen_portfolio_flutter/features/auth/data/datasources/auth_remote_data_source.dart';
